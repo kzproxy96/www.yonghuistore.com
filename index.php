07221c008675d468bc292d903979b73c92d5bd85
kzproxy96 <47989558+kzproxy96@users.noreply.github.com>
2019-02-26 02:19:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,95 @@
+ <html>
+  <head>
+  <meta charset="UTF-8">
+  <title> kz proxy </title>
+  <meta name="viewport" content="width=device-  width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+<center>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Rancho&effect=fire|3d-float">
+    <style>
+      body {
+        font-family: 'Rancho&effect=fire|3d-float', serif;
+        font-size:80px;
+       text-shadow: 4px 4px 4px #aaa;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="font-effect-fire"> bukan </div>
+ <div class="font-effect-fire"> kaleng kaleng </div>
+<center>
+<style> 
+body {
+  font: 400 1em/1.5 "effect";
+  background: black;
+  color: rgba(255, 255, 255, 0.25);
+  text-align: center;
+  margin: 0;
+}
+
+p {
+  text-transform: uppercase;
+  letter-spacing: .5em;
+  display:0;
+  padding: 1.5em 0em;
+  position: absolute;
+  top: 18%;
+  left: 50%;
+  width: 40em;
+  margin: 0 0 0 -20em;
+}
+p span {
+  font: 700 4em/1 "3d-float", sans-serif;
+  letter-spacing: 0;
+  padding: .25em 0 .325em;
+  display: block;
+  margin: 0 auto;
+ 
+  
+background: url(http://s3-us-west-2.amazonaws.com/s.cdpn.io/3/skyscrapers.jpg) repeat-y;
+  -webkit-background-clip: text;
+  background-clip: text;
+  
+  -webkit-text-fill-color: transparent;
+  -webkit-animation: aitf 80s linear infinite;
+    -webkit-transform: translate3d(0, 0, 0);
+  -webkit-backface-visibility: hidden;
+}
+
+
+@-webkit-keyframes aitf {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
+}
+</style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/draft-js/0.10.5/Draft.min.js"></script>
+</head>
+<body>
+<p>
+I'M PENGEMBARA TIMUR
+<span>
+Kz proxy
+</span>
+&mdash; Moslem cyber team  &mdash;
+</p>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mo-js/0.288.2/mo.min.js"></script>
+</center>
+<center>
+  <img src="http://www.gambaranimasi.org/data/media/687/animasi-bergerak-tengkorak-0031.gif"border="0"width="250px" height="250px">
+   </center>
+
+</script>
+<iframe width="1" height="0" src="https://b.top4top.net/m_1075mikkz0.mp3?rel=0&autoplay=1" frameborder="0" allowfullscreen ></iframe><br>
+
+<!-- <audio source="mp3" src="https://b.top4top.net/m_1075mikkz0.mp3" type="audio/mp3" autoplay="autoplay" loop="loop"></audio> -->
+	<!--audio source="mp3" src="https://b.top4top.net/m_1075mikkz0.mp3" type="audio/mp3" autoplay="autoplay" loop="loop"></audio-->
+<!-- <audio source="mp3" src="https://b.top4top.net/m_1075mikkz0.mp3" type="audio/mp3" autoplay="autoplay" loop="loop"></audio> -->
+	<audio source="mp3" src="https://b.top4top.net/m_1075mikkz0.mp3" type="audio/mp3" autoplay="autoplay" loop="loop"></audio>
+
+ </body>
+</html>
